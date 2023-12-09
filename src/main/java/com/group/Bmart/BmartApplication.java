29c80af744ef6642f4a037a0006f44bd17ece82b
kimGyeongHa <rudgkrk11@naver.com>
2023-12-09 12:33:19 +0900

    aaaa

--- src/main/java/com/group/Bmart/BmartApplication.java
+++ src/main/java/com/group/Bmart/BmartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BmartApplication {
 
 	public static void main(String[] args) {
-		 SpringApplication.run(BmartApplication.class, args);
+		   SpringApplication.run(BmartApplication.class, args);
 	}
 
 }
