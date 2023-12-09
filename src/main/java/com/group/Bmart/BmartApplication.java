0e8dbb6a651e81edcf3d3bb13e7cd7814213f765
So hyounsoo <so5663@naver.com>
2023-12-09 10:39:29 +0900

    first commit

---
+++ src/main/java/com/group/Bmart/BmartApplication.java
@@ -0,0 +1,13 @@
+package com.group.Bmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/Bmart/BmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.Bmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
