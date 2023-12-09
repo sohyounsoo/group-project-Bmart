13ad622a32be305d7ea2e1685fbe85847ed24e48
kimGyeongHa <rudgkrk11@naver.com>
2023-12-09 11:58:18 +0900

    git push

--- src/main/java/com/group/Bmart/conifg/SecurityConfig.java
+++ src/main/java/com/group/Bmart/conifg/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.group.Bmart.conifg;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
 public class SecurityConfig {
 
 }
