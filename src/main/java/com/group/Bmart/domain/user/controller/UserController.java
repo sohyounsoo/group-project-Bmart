447c065e8e4792f61e6d73db1a0d277854296591
So hyounsoo <so5663@naver.com>
2023-12-16 11:51:49 +0900

    Add CommonREsponse

--- src/main/java/com/group/Bmart/domain/user/controller/UserController.java
+++ src/main/java/com/group/Bmart/domain/user/controller/UserController.java
@@ -7,9 +7,12 @@ import com.group.Bmart.domain.user.service.response.FindUserDetailResponse;
 import com.group.Bmart.global.auth.LoginUser;
 //import com.group.Bmart.global.auth.oauth.client.OAuthRestClient;
 import com.group.Bmart.global.auth.oauth.client.OAuthRestClient;
+import com.group.Bmart.global.dto.CommonResponse;
+import com.group.Bmart.global.message.Message;
 import com.group.Bmart.global.util.ErrorTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,20 +25,39 @@ public class UserController {
     private final UserService userService;
     private final OAuthRestClient restClient;
 
+//    @GetMapping("/users/me")
+//    public ResponseEntity<FindUserDetailResponse> findUser(@LoginUser Long userId) {
+//        FindUserDetailResponse findUserDetailResponse =
+//            userService.findUser(FindUserCommand.from(userId));
+//        return ResponseEntity.ok(findUserDetailResponse);
+//    }
+
     @GetMapping("/users/me")
-    public ResponseEntity<FindUserDetailResponse> findUser(@LoginUser Long userId) {
+    public ResponseEntity<CommonResponse> findUser(@LoginUser Long userId) {
         FindUserDetailResponse findUserDetailResponse =
-            userService.findUser(FindUserCommand.from(userId));
-        return ResponseEntity.ok(findUserDetailResponse);
+                userService.findUser(FindUserCommand.from(userId));
+
+        CommonResponse response = CommonResponse.builder()
+                .success(true)
+                .response(findUserDetailResponse)
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/users/me")
-    public ResponseEntity<Void> deleteUser(@LoginUser Long userId) {
+    public ResponseEntity<CommonResponse> deleteUser(@LoginUser Long userId) {
         FindUserCommand findUserDetailCommand = FindUserCommand.from(userId);
         FindUserDetailResponse findUserDetailResponse = userService.findUser(findUserDetailCommand);
         restClient.callUnlinkOAuthUser(findUserDetailResponse);
         userService.deleteUser(userId);
-        return ResponseEntity.noContent().build();
+
+        CommonResponse response = CommonResponse.builder()
+                .success(true)
+                .response(Message.ACCOUNT_DELETED_SUCCESSFULLY)
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @ExceptionHandler(UserException.class)
