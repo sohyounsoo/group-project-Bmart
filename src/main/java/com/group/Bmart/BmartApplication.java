7af16c9677995bac4609a4e21e6fd19563b859b1
So hyounsoo <so5663@naver.com>
2024-01-13 16:55:32 +0900

    delete message

--- src/main/java/com/group/Bmart/BmartApplication.java
+++ src/main/java/com/group/Bmart/BmartApplication.java
@@ -9,7 +9,6 @@ public class BmartApplication {
 	//SpringBoot
 	 public static void main(String[] args) {
 		   SpringApplication.run(BmartApplication.class, args);
-       System.out.println("마지막으로 경고한다 이번엔 정봉찬이다.");
 	}
 
 }
