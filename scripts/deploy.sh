30ecb1fce1460089e1022040884ae5b7ff55a69c
So hyounsoo <so5663@naver.com>
2024-03-02 11:06:49 +0900

    ci: 스크립트 생성

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 현재 실행 중인 컨테이너 확인
+running_containers=$(docker-compose ps -q)
+
+# 실행 중인 컨테이너가 있으면 중지 및 삭제
+if [ -n "$running_containers" ]; then
+    echo "Stopping and removing existing containers..."
+    docker-compose down
+fi
+
+# Docker Compose 실행
+echo "Running Docker Compose..."
+docker-compose up -d
