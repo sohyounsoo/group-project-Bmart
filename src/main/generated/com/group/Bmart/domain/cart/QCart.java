ab0f810422ce583c83ba130ce85c1381990f086f
So hyounsoo <so5663@naver.com>
2024-03-22 02:06:53 +0900

    Add CategorySeriveTest

---
+++ src/main/generated/com/group/Bmart/domain/cart/QCart.java
@@ -0,0 +1,59 @@
+package com.group.Bmart.domain.cart;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QCart is a Querydsl query type for Cart
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QCart extends EntityPathBase<Cart> {
+
+    private static final long serialVersionUID = 1315816278L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QCart cart = new QCart("cart");
+
+    public final com.group.Bmart.global.QBaseTimeEntity _super = new com.group.Bmart.global.QBaseTimeEntity(this);
+
+    public final NumberPath<Long> cartId = createNumber("cartId", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public final com.group.Bmart.domain.user.QUser user;
+
+    public QCart(String variable) {
+        this(Cart.class, forVariable(variable), INITS);
+    }
+
+    public QCart(Path<? extends Cart> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QCart(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QCart(PathMetadata metadata, PathInits inits) {
+        this(Cart.class, metadata, inits);
+    }
+
+    public QCart(Class<? extends Cart> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.user = inits.isInitialized("user") ? new com.group.Bmart.domain.user.QUser(forProperty("user")) : null;
+    }
+
+}
+
