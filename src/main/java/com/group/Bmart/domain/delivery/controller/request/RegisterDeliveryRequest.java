4c77adaae4af4b072babd2885a6ca36a3efc40ac
so5663 <sohyounsoo@gmail.com>
2024-04-05 20:07:08 +0900

    ë°ãfeat: registerDelivery test

--- src/main/java/com/group/Bmart/domain/delivery/controller/request/RegisterDeliveryRequest.java
+++ src/main/java/com/group/Bmart/domain/delivery/controller/request/RegisterDeliveryRequest.java
@@ -6,8 +6,7 @@ import jakarta.validation.constraints.Positive;
 public record RegisterDeliveryRequest (
 
     @Positive(message = "배달 예상 소요 시간은 양수여야 합니다.")
-    @NotNull(message = "배달 예상 소요 시간은 필수입니다.")
-    Integer estimateMinutes) {
+    @NotNull(message = "배달 예상 소요 시간은 필수입니다.") Integer estimateMinutes) {
 
     }
 
--- src/test/java/com/group/Bmart/domain/delivery/controller/DeliveryControllerTest.java
+++ src/test/java/com/group/Bmart/domain/delivery/controller/DeliveryControllerTest.java
@@ -1,4 +1,60 @@
-import static org.junit.jupiter.api.Assertions.*;
-class DeliveryControllerTest {
-  
+package com.group.Bmart.domain.delivery.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.group.Bmart.base.BaseControllerTest;
+import com.group.Bmart.domain.delivery.controller.request.RegisterDeliveryRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+class DeliveryControllerTest extends BaseControllerTest {
+
+    @Test
+    @DisplayName("배달 생성 api 호출 시")
+    void registerDelivery() throws Exception {
+        //given
+        RegisterDeliveryRequest registerDeliveryRequest
+                = new RegisterDeliveryRequest(60);
+
+        given(deliveryService.registerDelivery(any())).willReturn(1L);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                post("/api/v1/orders/{orderId}/deliveries", 1L)
+                        .header(AUTHORIZATION, accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(registerDeliveryRequest))
+        );
+
+        //then
+        resultActions.andExpect(status().isCreated())
+            .andDo(restDocs.document(
+                requestHeaders(
+                        headerWithName(AUTHORIZATION).description("액세스 토큰")
+                ),
+                pathParameters(
+                        parameterWithName("orderId").description("주문 ID")
+                ),
+                requestFields(
+                        fieldWithPath("estimateMinutes").type(NUMBER).description("배달 예상 시간(분)")
+                ),
+                responseHeaders(
+                        headerWithName("Location").description("생성된 리소스 위치")
+                )
+            ));
+
+    }
+
 }
